5c9998a85f3ddc213d3b1408737c5b0133bb882c
Marcio <marciohamer@gmail.com>
2022-08-31 22:15:19 -0700

    ddd

--- node/api/index.js
+++ node/api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const mysql = require('mysql');
 const app = express()
+const axios = require('axios')
 
 app.use(cors())
 app.use(express.json())
@@ -16,7 +17,7 @@ const connection = mysql.createConnection({
 })
 
 app.get('/', (req, res) => { 
-   connection.query(`SELECT * FROM PEOPLE;`,  (err, rows, fields) => {
+   connection.query(`SELECT * FROM PEOPLE;`,  (err, rows,) => {
      if(err !=null){
       throw err;
      }  
@@ -24,19 +25,35 @@ app.get('/', (req, res) => {
    console.log(req.headers.host)
    return res.send({'rows' : rows})
   })
+
 })
 
-app.post('/', (req, res) => {
-    const nome = req.body.nome
-    connection.query(`INSERT INTO PEOPLE(nome) VALUES('${nome}');`, (err, rows, fields) => {
-        if(err != null){
-          throw err;
-        }
-    console.log(req.headers.host)
-    return res.send('Ok')
-    })
+app.post('/insert_peoples', async(req, res) => {
+  console.log('AKIII')  
+  const {data} = await axios.get('https://swapi.dev/api/people')
+  console.log(data, 'data')
+  console.log(data.results)
+  data.results.forEach(element => {
+    connection.query(`INSERT INTO PEOPLE(nome, height, mass, hair_color, skin_color, eye_color, birth_year, gender, created, edited, url) VALUES(
+      '${element.name}',
+      '${element.height}',
+      '${element.mass}',
+      '${element.hair_color}',
+      '${element.skin_color}',
+      '${element.eye_color}',
+      '${element.birth_year}',
+      '${element.gender}',
+      '${element.created}',
+      '${element.edited}',
+      '${element.url}'
+      );`, (err, rows,) => {
+      if(err != null){
+        throw err;
+      }
+  });
 })
-
+return res.send('Ok')
+})
 app.delete('/:id', (req, res) => {
     connection.query(`DELETE FROM PEOPLE WHERE id="${req.params.id}";`,(err, rows, fields) => {
       if(err !=null){
